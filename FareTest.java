cfd7e0ca6d0efc89ae0b254188622d4a36a09d10
akshdeepkaur2 <39845021+akshdeepkaur2@users.noreply.github.com>
2019-05-27 12:52:33 -0400

    R3- red

--- FareTest.java
+++ FareTest.java
@@ -16,10 +16,29 @@ public class FareTest {
 //R1
 	@Test
 	public void test1() {
-		String[]from  = new String[] {"Leslie"};
-		String[] to = new String[]{"Don Mills"};
-		Double fare = z.calculateTotal(from,to);
+		String[]from1  = new String[] {"Leslie"};
+		String[] to1 = new String[]{"Don Mills"};
+		String[] z = new String[] {""};
+		Double fare = z.calculateTotal(from1,to1);
 		assertEquals(2.5, fare,0.0);
 	}
+	//R2
+	@Test
+	public void test2() {
+		String[]from1  = new String[] {"Sheppard"};
+		String[] to1 = new String[]{"Finch Station"};
+		String[] z = new String[] {""};
+		Double fare = z.calculateTotal(from1,to1);
+		assertEquals(3.00, fare,0.0);
+	}
+	//R3
+	
+	@Test
+	public void test3() {
+		String[]from1  = new String[] {"Don Mills"};
+		String[] to1 = new String[]{"Finch Station"};
+		String[] z = new String[] {"calculate"};
+		Double fare = z.calculateTotal(from1,to1);
+		assertEquals(3.00, fare,0.0);
 
 }
